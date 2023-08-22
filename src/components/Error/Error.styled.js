ff1e7779b80db4d81109ad63f0353a2a6cf02bef
Tanyapavliuk <tanyapavlyk1998@gmail.com>
2023-08-22 19:26:01 +0300

    add 404 page in public directory, script in index

--- src/components/Error/Error.styled.js
+++ src/components/Error/Error.styled.js
@@ -1,9 +1,11 @@
 import styled from '@emotion/styled';
 
 export const TextError = styled.li`
+  margin-top: 20px;
   font-size: 20px;
   padding: 20px;
   text-align: center;
   border: 1px solid #e13c52;
   border-radius: 5px;
+  list-style-type: none;
 `;
